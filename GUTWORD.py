361a814039beceffa88793a5290642b1f5e3b3f6
Fwldom <111840687+fwldom@users.noreply.github.com>
2023-06-13 12:33:15 +0330

    F

---
+++ GUTWORD.py
@@ -0,0 +1,30 @@
+import random
+ 
+Plangs = ("python", "php", "javascript", "c", "perl", "c++", "java", "ruby", "c#", "visual basic", "fortran")
+ 
+answer = random.choice(Plangs)
+ 
+correct = answer
+ 
+rumble = ""
+ 
+while answer:
+    position = random.randrange(len(answer))
+    rumble += answer[position]
+    answer = answer[:position] + answer[(position + 1):]
+ 
+print("The Word Is: python , php , javascript , c , perl , c++ , java , ruby , CSharp , visual basic , fortran")
+ 
+guess = input("Guess This Programming Language:")
+guess = guess.lower()
+ 
+while (guess != correct) or (guess == ""):
+    print("That is not the correct answer")
+    guess = input("Guess This Programming Language:")
+    guess = guess.lower()
+ 
+if guess == correct:
+    print("Congratulation You Win!")
+    input("\n\n Press enter to exit")
+
+#Sourceiran.com
